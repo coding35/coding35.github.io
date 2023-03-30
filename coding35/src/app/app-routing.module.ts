8b804413f2d2aa2dc83f7092a3be176314f2a989
adam <imamreed@outlook.com>
2023-03-29 19:22:18 -0500

    content

--- coding35/src/app/app-routing.module.ts
+++ coding35/src/app/app-routing.module.ts
@@ -14,6 +14,8 @@ const routes: Routes = [
   { path: 'design', component: ContentListComponent, data: { page: 'design' } },
   { path: 'coding/:id', component: PageComponent, data: { page: 'coding' } },
   { path: 'coding', component: ContentListComponent, data: { page: 'coding' } },
+  { path: 'database/:id', component: PageComponent, data: { page: 'database' } },
+  { path: 'database', component: ContentListComponent, data: { page: 'database' } },
   { path: 'video/:id', component: PageComponent, data: { page: 'video' } },
   { path: 'video', component: ContentListComponent, data: { page: 'video' } },
   { path: 'productivity/:id', component: PageComponent, data: { page: 'productivity' } },
@@ -28,8 +30,8 @@ const routes: Routes = [
   { path: 'about', component: AboutComponent, data: { page: 'about' } },
   { path: 'contact', component: ContactComponent, data: { page: 'contact' } },
   { path: 'tree', component: ForceDirectedTreeComponent, data: { page: 'tree' } },
-  { path: '**', component: ContentListComponent, data: { page: 'architecture' } },
-  { path: '', component: ContentListComponent, data: { page: 'architecture' } }
+  { path: '**', component: ContentListComponent, data: { page: ''} },
+  { path: '', component: ContentListComponent, data: { page: '' } }
 ];
 
 @NgModule({
