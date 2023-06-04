96f3ff95c74ba9a5e5630205ae1ef01602f5f358
adam <imamreed@outlook.com>
2023-06-04 14:00:18 -0500

    content

--- coding35/src/app/shared/service/idb-storage-access.service.ts
+++ coding35/src/app/shared/service/idb-storage-access.service.ts
@@ -8,7 +8,7 @@ export class IdbStorageAccessService {
   data: IContentModel[] = [];
 
   private readonly database = 'Coding35';
-  private readonly version = 4;
+  private readonly version = 5;
   private readonly store = 'ContentStore';
 
   constructor(private windowObj: Window) {
